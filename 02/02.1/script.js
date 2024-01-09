f8890a2dd5c0e92a89fffca8b07d47fd3e10f485
lucashenriqramos <lucashenriquesramos@gmail.com>
2024-01-08 21:52:16 -0300

    02.1 half complete

--- 02/02.1/script.js
+++ 02/02.1/script.js
@@ -1,2 +1,14 @@
-var name = prompt('What is your name?')
-alert('It is a pleasure to meet you, ' + name + '!')
+confirm('Oh Hi!, is there someone out here?')
+confirm('Youre still here?')
+var name = prompt('Ok then, so whats your name?')
+alert('Its so nice to have u here, ' + name + '!')
+
+var year = 2024
+var ybirth = prompt('Which year did u were born?')
+var subtract = year - ybirth
+confirm('So that means youre going, ' + subtract + ' this year, right?')
+alert('Oh, a good time has passed! hope youre enojying life :)')
+var plus = parseFloat(prompt('Now type any number u think of'))
+var plus1 = parseFloat(prompt('Again...'))
+var sum = plus + plus1
+alert('The sum of the numbers u thought of is: ' + sum + '.')
